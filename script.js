098095050f16b07a394248d08d10292f496fe0ef
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 22:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 //your JS code here. If required.
-document.querySelectorAll('.btn');
-let sounds=[];
-document.getElementById("appl").addEventListener('click',()=>{ let sound = new Audio('./sounds/appl.mp3');
-															 sounds.push(sound);
-															  sound.currentTime = 0;
-															  sound.play();});
-document.getElementById("boo").addEventListener('click',()=>{let sound=new Audio('./sounds/boo.mp3');
-															 sounds.push(sound);
-															 sound.currentTime = 0;
-															 sound.play();});
-document.getElementById("gasp").addEventListener('click',()=>{let sound=new Audio('./sounds/gasp.mp3');
-															 sounds.push(sound);
-															  sound.currentTime = 0;
-															  sound.play();});
-document.getElementById("toda").addEventListener('click',()=>{let sound=new Audio('./sounds/toda.mp3');
-															 sounds.push(sound);
-															  sound.currentTime = 0;
-															  sound.play();});
-document.getElementById("victor").addEventListener('click',()=>{let sound=new Audio('./sounds/victor.mp3');
-															 sounds.push(sound);
-																sound.currentTime = 0;
-																sound.play();});
-document.getElementById("wrong").addEventListener('click',()=>{let sound=new Audio('./sounds/wrong.mp3');
-															sounds.push(sound);
-															   sound.currentTime = 0;
-															   sound.play();});
-document.getElementById("stop").addEventListener('click',()=>{sounds.forEach(sound =>
-															  sound.pause());});
+window.onload = function(){
+    let sounds = {
+        appl: new Audio('./sounds/appl.mp3'),
+        boo: new Audio('./sounds/boo.mp3'),
+        gasp: new Audio('./sounds/gasp.mp3'),
+        toda: new Audio('./sounds/toda.mp3'),
+        victor: new Audio('./sounds/victor.mp3'),
+        wrong: new Audio('./sounds/wrong.mp3')
+    }
+document.getElementById("appl").addEventListener('click',()=>{ 
+    sounds.appl.currentTime = 0;
+    sounds.appl.play();
+});
+document.getElementById("boo").addEventListener('click',()=>{ 
+    sounds.boo.currentTime = 0;
+    sounds.boo.play();
+});
+document.getElementById("gasp").addEventListener('click',()=>{ 
+    sounds.gasp.currentTime = 0;
+    sounds.gasp.play();
+});
+document.getElementById("toda").addEventListener('click',()=>{ 
+    sounds.toda.currentTime = 0;
+    sounds.toda.play();
+});
+document.getElementById("victor").addEventListener('click',()=>{ 
+    sounds.victor.currentTime = 0;
+    sounds.victor.play();
+});
+document.getElementById("wrong").addEventListener('click',()=>{ 
+    sounds.wrong.currentTime = 0;
+    sounds.wrong.play();
+});
+document.getElementById("stop").addEventListener('click',()=>{
+        for (let sound in sounds) {
+            sounds[sound].pause();
+        }
+    });
+}
