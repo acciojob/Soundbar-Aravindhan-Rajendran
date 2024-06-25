3e049828882efadca91e92953f0ff5aa7c744da9
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 22:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-//your JS code here. If required.
 window.onload = function(){
     let sounds = {
-        appl: new Audio('./sounds/appl.mp3'),
+        applause: new Audio('./sounds/appl.mp3'),
         boo: new Audio('./sounds/boo.mp3'),
         gasp: new Audio('./sounds/gasp.mp3'),
         toda: new Audio('./sounds/toda.mp3'),
-        victor: new Audio('./sounds/victor.mp3'),
+        victory: new Audio('./sounds/victor.mp3'),
         wrong: new Audio('./sounds/wrong.mp3')
     }
-document.getElementById("appl").addEventListener('click',()=>{ 
-    sounds.appl.currentTime = 0;
-    sounds.appl.play();
+document.getElementById("applause").addEventListener('click',()=>{ 
+    sounds.applause.currentTime = 0;
+    sounds.applause.play();
 });
 document.getElementById("boo").addEventListener('click',()=>{ 
     sounds.boo.currentTime = 0;
@@ -24,9 +23,9 @@ document.getElementById("toda").addEventListener('click',()=>{
     sounds.toda.currentTime = 0;
     sounds.toda.play();
 });
-document.getElementById("victor").addEventListener('click',()=>{ 
-    sounds.victor.currentTime = 0;
-    sounds.victor.play();
+document.getElementById("victory").addEventListener('click',()=>{ 
+    sounds.victory.currentTime = 0;
+    sounds.victory.play();
 });
 document.getElementById("wrong").addEventListener('click',()=>{ 
     sounds.wrong.currentTime = 0;
