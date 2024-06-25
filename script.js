4266601a917dcb876e3de9cf3f2a1c4803f90c73
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 22:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.querySelectorAll('.btn');
+let sounds=[];
+document.getElementById("appl").addEventListener('click',()=>{ let sound = new Audio('./sounds/appl.mp3');
+															 sounds.push(sound);
+															  sound.play();});
+document.getElementById("boo").addEventListener('click',()=>{let sound=new Audio('./sounds/boo.mp3');
+															 sounds.push(sound);
+															 sound.play();});
+document.getElementById("gasp").addEventListener('click',()=>{let sound=new Audio('./sounds/gasp.mp3');
+															 sounds.push(sound);
+															  sound.play();});
+document.getElementById("toda").addEventListener('click',()=>{let sound=new Audio('./sounds/toda.mp3');
+															 sounds.push(sound);
+															  sound.play();});
+document.getElementById("victor").addEventListener('click',()=>{let sound=new Audio('./sounds/victor.mp3');
+															 sounds.push(sound);
+																sound.play();});
+document.getElementById("wrong").addEventListener('click',()=>{let sound=new Audio('./sounds/wrong.mp3');
+															sounds.push(sound);
+															   sound.play();});
+document.getElementById("stop").addEventListener('click',()=>{sounds.forEach(sound =>
+															  sound.pause());});
