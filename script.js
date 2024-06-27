8e32baf0a2a276b18823bec67a3877c853811fc1
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-27 18:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let sounds = {
-        applause: new Audio('../sounds/applause.mp3'),
-        boo: new Audio('../sounds/boo.mp3'),
-        gasp: new Audio('../sounds/gasp.mp3'),
-        toda: new Audio('../sounds/toda.mp3'),
-        victory: new Audio('../sounds/victory.mp3'),
-        wrong: new Audio('../sounds/wrong.mp3')
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+    const audioPlayer = document.getElementById('audioPlayer');
+ 
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const soundFile = this.getAttribute('data-sound');
+            playSound(soundFile);
+        });
+    });
+ 
+    stopButton.addEventListener('click', function() {
+        stopSound();
+    });
+ 
+    function playSound(soundFile) {
+        audioPlayer.src = soundFile;
+        audioPlayer.play();
     }
-document.getElementById("applause").addEventListener('click', function() { 
-        sounds.applause.currentTime = 0;
-        sounds.applause.play();
-    });
-document.getElementById("boo").addEventListener('click',function(){ 
-    sounds.boo.currentTime = 0;
-    sounds.boo.play();
+ 
+    function stopSound() {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
+    }
 });
-document.getElementById("gasp").addEventListener('click',function(){ 
-    sounds.gasp.currentTime = 0;
-    sounds.gasp.play();
-});
-document.getElementById("toda").addEventListener('click',function(){ 
-    sounds.toda.currentTime = 0;
-    sounds.toda.play();
-});
-document.getElementById("victory").addEventListener('click',function(){ 
-    sounds.victory.currentTime = 0;
-    sounds.victory.play();
-});
-document.getElementById("wrong").addEventListener('click',function(){ 
-    sounds.wrong.currentTime = 0;
-    sounds.wrong.play();
-});
-document.getElementById("stop").addEventListener('click',function(){
-        for (let sound in sounds) {
-            sounds[sound].pause();
-        }
-    });
-						  });
